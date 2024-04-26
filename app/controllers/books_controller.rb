0ab19b5512eb9d12f796cb0e731c76266a160fc2
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-04-26 14:05:05 +0000

    [finish]Bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,25 +2,35 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:success] = "You have created book successfully."
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
-      render 'index'
+      flash.now[:alert] = @book.errors.full_messages.join(", ")
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    unless current_user.id == @book.user_id
+      redirect_to books_path
+    end
   end
 
   def update
@@ -28,19 +38,26 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
+      flash[:error] = "error to update the book."
       render "edit"
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+    if current_user == @book.user
+      @book.destroy
+      flash[:success] = "Book was successfully deleted."
+      redirect_to books_path
+    else
+      flash[:alert] = "Failed to delete the book."
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ApplicationRecord
-  has_many :user
+  belongs_to :user
+  
+  def get_image
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    image
+  end
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
