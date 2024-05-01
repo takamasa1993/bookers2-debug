8f9f8113d7e0507b58151c0cdaf7e0945891f6e1
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-05-01 17:14:48 +0000

    [add follow_view]bookers2-debug

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,24 +1,33 @@
 class RelationshipsController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
     @followers_count = @user.followers.count
     @following_count = @user.following.count
   end
-  
+
   def index
   end
-  
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.followings << @user
     redirect_to request.referer
   end
-  
+
   def destroy
     @relationship = Relationship.find(params[:id])
     @relationship.destroy
     redirect_to request.referer
   end
-  
+
+  def followings
+    user = User.find(params[:id])
+		@users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+		@users = user.followers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,21 @@ Rails.application.routes.draw do
   get '/home/about', to: 'homes#about'
   root to: "homes#top"
   devise_for :users
+  
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-  resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:index,:show,:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-   resources :users, only: [:show] do
+  resources :users, only: [:show] do
     member do
       post 'follow' => 'relationships#create'
       delete 'unfollow' => 'relationships#destroy'
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
     end
   end
 end
