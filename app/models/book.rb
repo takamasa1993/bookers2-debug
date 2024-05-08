1eae6a301883d3daba0c8ddde3af2adabfce0d50
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-05-08 12:08:32 +0000

    [start hidouki]bookers2-debug

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
   
   def get_image
